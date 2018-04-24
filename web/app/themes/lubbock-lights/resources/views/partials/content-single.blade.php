71f1d0e52e73140bcdaaa38308ffaaf21bb7740f
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-04-24 11:34:53 -0500

    Removed Who Said That? category

--- web/app/themes/lubbock-lights/resources/views/partials/content-single.blade.php
+++ web/app/themes/lubbock-lights/resources/views/partials/content-single.blade.php
@@ -8,8 +8,12 @@
     @include('partials/entry-meta')
   </header>
   <div class="entry-content">
-    @php the_post_thumbnail( 'full' ) @endphp
-    @php the_content() @endphp
+    <div class="entry-content__image">
+      @php the_post_thumbnail( 'full' ) @endphp
+    </div>
+    <div class="entry-content__body">
+      @php the_content() @endphp
+    </div>
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
