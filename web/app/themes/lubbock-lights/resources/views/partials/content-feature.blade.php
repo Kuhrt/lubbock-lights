0fde47f9d43c16bc1e3e064bce71a5e8462b1166
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-05-02 14:45:13 -0500

    Changed feature article title placement. Shrunk down series title

--- web/app/themes/lubbock-lights/resources/views/partials/content-feature.blade.php
+++ web/app/themes/lubbock-lights/resources/views/partials/content-feature.blade.php
@@ -1,22 +1,24 @@
 <article @php post_class('section-feature') @endphp>
   @php $featureImageUrl = get_the_post_thumbnail_url(get_the_ID(), 'full') @endphp
   <div class="entry-image" style="background-image: url('@php echo $featureImageUrl @endphp');"></div>
-  <p class="entry-category"><a href="{{ get_category_link(get_cat_ID( get_the_category()[0]->cat_name )) }}">@php echo get_the_category()[0]->cat_name @endphp</a></p>
-  <header>
-    @if (get_field('series_title'))
-      <div class="entry-subtitle">
-        <p>{{ the_field('series_title') }}</p>
-        @if (get_field('series_icon'))
-          <div class="entry-subtitle__image"><div style="background-image: url('{{ the_field('series_icon') }}');"></div></div>
-        @endif
-      </div>
-    @endif
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    <p class="byline author vcard">
-      {{ __('By', 'sage') }} {{ get_the_author() }}
-    </p>
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
+  <div class="entry-info">
+    <p class="entry-category"><a href="{{ get_category_link(get_cat_ID( get_the_category()[0]->cat_name )) }}">@php echo get_the_category()[0]->cat_name @endphp</a></p>
+    <header>
+      @if (get_field('series_title'))
+        <div class="entry-subtitle">
+          <p>{{ the_field('series_title') }}</p>
+          @if (get_field('series_icon'))
+            <div class="entry-subtitle__image"><div style="background-image: url('{{ the_field('series_icon') }}');"></div></div>
+          @endif
+        </div>
+      @endif
+      <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
+      <p class="byline author vcard">
+        {{ __('By', 'sage') }} {{ get_the_author() }}
+      </p>
+    </header>
+    <div class="entry-summary">
+      @php the_excerpt() @endphp
+    </div>
   </div>
 </article>
